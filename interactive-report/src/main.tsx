4931afa29680234826be382325673cc07e8d2cb6
oarabilekoore <oarabilekoore@protonmail.com>
2025-12-09 05:40:34 +0200

    updates

--- interactive-report/src/main.tsx
+++ interactive-report/src/main.tsx
@@ -5,10 +5,11 @@ import "./main.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./pages/+Layout";
 import Article from "./pages/article";
+import "katex/dist/katex.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/dfm-feasibility/">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Article />} />
--- interactive-report/vite.config.ts
+++ interactive-report/vite.config.ts
@@ -3,9 +3,24 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@mdx-js/rollup";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
-  plugins: [{ enforce: "pre", ...mdx() }, react(), tailwindcss()],
+  plugins: [
+    {
+      enforce: "pre",
+      ...mdx({
+        remarkPlugins: [remarkMath],
+
+        rehypePlugins: [rehypeKatex],
+
+        providerImportSource: "@mdx-js/react",
+      }),
+    },
+    react(),
+    tailwindcss(),
+  ],
   base: "/dfm-feasibility/",
   resolve: {
     alias: {
