78b1716be84ea1e5a79960f411bdb2706e03be11
oarabilekoore <oarabilekoore@protonmail.com>
2025-12-09 18:57:44 +0200

    Changed from react block expression to dangerouslySetInnerHTML and katex raw string rendering.

--- interactive-report/src/components/report/math-text.tsx
+++ interactive-report/src/components/report/math-text.tsx
@@ -1,17 +1,20 @@
-import { InlineMath, BlockMath } from "react-katex";
 import "katex/dist/katex.min.css";
+import katex from "katex";
 
 interface MathProps {
   expression: string;
   displayMode?: boolean;
 }
 
-export default function MathDisplay({
-  expression,
-  displayMode = true,
-}: MathProps) {
-  if (displayMode) {
-    return <BlockMath math={expression} />;
-  }
-  return <InlineMath math={expression} />;
+export default function MathDisplay({ expression }: MathProps) {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: katex.renderToString(expression, {
+          throwOnError: true,
+          displayMode: true,
+        }),
+      }}
+    />
+  );
 }
